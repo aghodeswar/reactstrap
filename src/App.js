138f21da10f4b14f5254722d3f12cd0504d72ee2
Anand Ghodeswar <anandghodeswar@ansh.com>
2019-09-06 15:42:01 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ function App() {
           <div className="row py-4">
             <div className="col-md-3 sidebar">
               <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action bg-light rounded-0">Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light rounded-0">Status</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light rounded-0">Dashboard</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light">Overview</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light">Events</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light">Profile</a>
+                <a href="/#" class="list-group-item list-group-item-action bg-light rounded-0">Status</a>
               </div>
             </div>
             <div className="col-md-9">
-              {/* <h3>Deploying a React App to GitHub Pages</h3> */}
+              <h3>Deploying a React single page App to GitHub Pages</h3>
               <p>You can configure GitHub Pages to publish your site's source files from master, gh-pages, or a /docs folder on your master branch for Project Pages and other Pages sites that meet certain criteria.</p>
 
               <p><i class="fa fa-github" aria-hidden="true"></i>  <a href="https://github.com/aghodeswar/reactstrap/tree/react-gh-pages">View on GitHub</a></p>
