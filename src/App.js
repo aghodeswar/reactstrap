d47242956354762a7f87215ebf96f6c65f73695b
Anand Ghodeswar <anandghodeswar@ansh.com>
2019-09-06 15:26:11 +0530

    added page and html code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">      
-        <div className="container">
-          <div className="col-md-12">gh-page</div>
-        </div>      
+    <div className="App">
+      <header>
+        <nav className="fixed-nav-wrapper">
+
+        </nav>
+      </header>
+      <div className="wrapper nav-spacer">
+        <div className="main-container">
+          <div className="row py-4">
+            <div className="col-md-3 sidebar">
+              <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action bg-light rounded-0">Dashboard</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light rounded-0">Status</a>
+              </div>
+            </div>
+            <div className="col-md-9">
+              {/* <h3>Deploying a React App to GitHub Pages</h3> */}
+              <p>You can configure GitHub Pages to publish your site's source files from master, gh-pages, or a /docs folder on your master branch for Project Pages and other Pages sites that meet certain criteria.</p>
+
+              <p>for source code you can visit this demo repo : <a href="https://github.com/aghodeswar/reactstrap/tree/react-gh-pages">https://github.com/aghodeswar/reactstrap/tree/react-gh-pages</a></p>
+
+
+              <p>for more details tutorial you can visit: <a href="https://github.com/gitname/react-gh-pages">https://github.com/gitname/react-gh-pages</a></p>              
+            </div>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
